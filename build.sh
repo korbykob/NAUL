bb9b56c892488990e0b08a9de3477776e71b0e90
korbykob <korbykobs@gmail.com>
2026-01-06 21:41:04 +0000

    Allow for addr2line in build script

--- build.sh
+++ build.sh
@@ -5,28 +5,32 @@ if [ ! -f "os/gnu-efi/x86_64/gnuefi/crt0-efi-x86_64.o" ]; then
     make -C os/gnu-efi
 fi
 
-flags="-Iinclude -ffreestanding -fno-stack-protector -fno-stack-check -mno-red-zone -maccumulate-outgoing-args"
-kernelFlags="$flags -Ios/include -fpic -c"
-programFlags="$flags -Iprograms/include -fno-pic -fno-pie -mcmodel=large -static -no-pie -nostartfiles -Wl,-Tprograms/linker.ld,-znoexecstack"
+cflags="-Iinclude -ffreestanding -fno-stack-protector -fno-stack-check -mno-red-zone -maccumulate-outgoing-args -g"
+kernelCflags="$cflags -Ios/include -fpic -c"
+programCflags="$cflags -Iprograms/include -static -fno-pic -fno-pie -mcmodel=large -c"
+
+lflags="-znoexecstack"
+kernellLflags="$lflags -shared -Bsymbolic -Los/gnu-efi/x86_64/lib -Los/gnu-efi/x86_64/gnuefi -Tos/gnu-efi/gnuefi/elf_x86_64_efi.lds os/gnu-efi/x86_64/gnuefi/crt0-efi-x86_64.o"
+programLflags="$lflags -Tprograms/linker.ld -no-pie"
 
 mkdir -p os/bin
-gcc $kernelFlags -fshort-wchar -Ios/gnu-efi/inc os/src/bootloader.c -o os/bin/bootloader.o
-gcc $kernelFlags os/src/serial.c -o os/bin/serial.o
-gcc $kernelFlags os/src/allocator.c -o os/bin/allocator.o
-gcc $kernelFlags os/src/filesystem.c -o os/bin/filesystem.o
-gcc $kernelFlags os/src/symbols.c -o os/bin/symbols.o
-gcc $kernelFlags os/src/gdt.c -o os/bin/gdt.o
-gcc $kernelFlags os/src/idt.c -o os/bin/idt.o
-gcc $kernelFlags os/src/terminal.c -o os/bin/terminal.o
-gcc $kernelFlags os/src/paging.c -o os/bin/paging.o
-gcc $kernelFlags os/src/syscalls.c -o os/bin/syscalls.o
-gcc $kernelFlags os/src/pic.c -o os/bin/pic.o
-gcc $kernelFlags os/src/scheduler.c -o os/bin/scheduler.o
-gcc $kernelFlags os/src/hpet.c -o os/bin/hpet.o
-gcc $kernelFlags os/src/keyboard.c -o os/bin/keyboard.o
-gcc $kernelFlags os/src/kernel.c -o os/bin/kernel.o
+gcc $kernelCflags -fshort-wchar -Ios/gnu-efi/inc os/src/bootloader.c -o os/bin/bootloader.o
+gcc $kernelCflags os/src/serial.c -o os/bin/serial.o
+gcc $kernelCflags os/src/allocator.c -o os/bin/allocator.o
+gcc $kernelCflags os/src/filesystem.c -o os/bin/filesystem.o
+gcc $kernelCflags os/src/symbols.c -o os/bin/symbols.o
+gcc $kernelCflags os/src/gdt.c -o os/bin/gdt.o
+gcc $kernelCflags os/src/idt.c -o os/bin/idt.o
+gcc $kernelCflags os/src/terminal.c -o os/bin/terminal.o
+gcc $kernelCflags os/src/paging.c -o os/bin/paging.o
+gcc $kernelCflags os/src/syscalls.c -o os/bin/syscalls.o
+gcc $kernelCflags os/src/pic.c -o os/bin/pic.o
+gcc $kernelCflags os/src/scheduler.c -o os/bin/scheduler.o
+gcc $kernelCflags os/src/hpet.c -o os/bin/hpet.o
+gcc $kernelCflags os/src/keyboard.c -o os/bin/keyboard.o
+gcc $kernelCflags os/src/kernel.c -o os/bin/kernel.o
 
-ld -shared -Bsymbolic -Los/gnu-efi/x86_64/lib -Los/gnu-efi/x86_64/gnuefi -Tos/gnu-efi/gnuefi/elf_x86_64_efi.lds -znoexecstack os/gnu-efi/x86_64/gnuefi/crt0-efi-x86_64.o \
+ld $kernellLflags \
 os/bin/bootloader.o \
 os/bin/serial.o \
 os/bin/allocator.o \
@@ -48,7 +52,9 @@ objcopy -j .text -j .sdata -j .data -j .rodata -j .dynamic -j .dynsym  -j .rel -
 nm -n os/bin/os.efi > os/bin/symbols.txt
 
 mkdir -p programs/bin/shell
-gcc $programFlags programs/src/shell/shell.c -o programs/bin/shell/shell.bin
+gcc $programCflags programs/src/shell/shell.c -o programs/bin/shell/shell.o
+ld $programLflags programs/bin/shell/shell.o -o programs/bin/shell/shell.bin
 
 mkdir -p programs/bin/test
-gcc $programFlags programs/src/test/test.c -o programs/bin/test/test.bin
+gcc $programCflags programs/src/test/test.c -o programs/bin/test/test.o
+ld $programLflags programs/bin/test/test.o -o programs/bin/test/test.bin
