83eac26789036e6593b06e97ed96cf4ca2ab0dad
korbykob <korbykobs@gmail.com>
2026-01-07 21:44:28 +0000

    Split uefi-run args into multiple lines

--- run.sh
+++ run.sh
@@ -4,4 +4,9 @@ set -e
 ./build.sh
 clear
 
-uefi-run -b OVMF-pure-efi.fd -d -f os/bin/symbols.txt -f os/font.psf -f programs/bin/shell/shell.bin -f programs/bin/test/test.bin os/bin/os.efi -- -enable-kvm -m 4G -cpu host -serial null -serial null -serial stdio -display sdl
+uefi-run -b OVMF-pure-efi.fd -d \
+-f os/bin/symbols.txt \
+-f os/font.psf \
+-f programs/bin/shell/shell.bin \
+-f programs/bin/test/test.bin \
+uefi-run -b OVMF-pure-efi.fd -d -f os/bin/symbols.txt -f os/font.psf -f programs/bin/shell/shell.bin -f programs/bin/test/test.bin os/bin/os.efi -- -enable-kvm -m 4G -cpu host -serial null -serial null -serial stdio -display sdlos/bin/os.efi -- -enable-kvm -cpu host -serial null -serial null -serial stdio -display sdl
