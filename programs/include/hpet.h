8f784881809957908fc56d27e5a59c8bc4442393
korbykob <korbykobs@gmail.com>
2026-02-06 20:43:46 +0000

    Update defines

--- programs/include/hpet.h
+++ programs/include/hpet.h
@@ -4,8 +4,9 @@
 #include <definitions.h>
 #include <calls.h>
 
-#define femtosecondsPerSecond 1000000000000000
-#define femtosecondsPerMicrosecond 1000000000
+#define FEMTOSECONDS_PER_SECOND 1000000000000000
+#define FEMTOSECONDS_PER_MILLISECOND 1000000000000
+#define FEMTOSECONDS_PER_MICROSECOND 1000000000
 
 static inline uint64_t getFemtoseconds()
 {
