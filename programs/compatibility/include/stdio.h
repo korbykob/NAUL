f6a692cb840f2fef24184a5572546d102bcb7af1
korbykob <korbykobs@gmail.com>
2026-08-16 03:04:50 +0100

    Add more functions to compatibility layer

--- programs/compatibility/include/stdio.h
+++ programs/compatibility/include/stdio.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <processes.h>
 #include <tty.h>
+#include <filesystem.h>
 
 #define NANOPRINTF_USE_FIELD_WIDTH_FORMAT_SPECIFIERS 1
 #define NANOPRINTF_USE_PRECISION_FORMAT_SPECIFIERS 1
@@ -16,8 +18,18 @@
 #define NANOPRINTF_IMPLEMENTATION
 #include "nanoprintf.h"
 
+#define exit(status) quit();
+#define FILE void
 #define fflush(stream) ({ 0; })
 #define fprintf(stream, format, ...) printf(format, __VA_ARGS__)
+#define vprintf(format, args) npf_vpprintf(nanoPut, 0, format, args)
+
+static struct
+{
+    uint8_t* memory;
+    uint64_t size;
+    uint64_t offset;
+} openFiles[5];
 
 static void nanoPut(int c, void *ctx)
 {
@@ -31,4 +43,38 @@ static int printf(const char* format, ...)
     int value = npf_vpprintf(nanoPut, 0, format, args);
     va_end(args);
     return value;
+}
+
+static void* fopen(const char* filename, const char* mode)
+{
+    if (checkFile(filename))
+    {
+        uint64_t id = 0;
+        while (openFiles[id].memory)
+        {
+            id++;
+        }
+        openFiles[id].offset = 0;
+        openFiles[id].memory = getFile(filename, &openFiles[id].size);
+        return (void*)(id + 1);
+    }
+    return 0;
+}
+
+static void fclose(void* handle)
+{
+    openFiles[(uint64_t)handle - 1].memory = 0;
+}
+
+static int fread(void* buf, uint64_t size, uint64_t count, void* handle)
+{
+    int bytes = min(openFiles[(uint64_t)handle - 1].offset + (size * count), openFiles[(uint64_t)handle - 1].size) - openFiles[(uint64_t)handle - 1].offset;
+    uint8_t* source = openFiles[(uint64_t)handle - 1].memory + openFiles[(uint64_t)handle - 1].offset;
+    uint8_t* destination = (uint8_t*)buf;
+    for (int i = 0; i < bytes; i++)
+    {
+        *destination++ = *source++;
+    }
+    openFiles[(uint64_t)handle - 1].offset += bytes;
+    return bytes;
 }
--- programs/compatibility/include/string.h
+++ programs/compatibility/include/string.h
@@ -2,7 +2,10 @@
 
 #include <str.h>
 #include <mem.h>
+#include <str.h>
 
 #define strcmp(a, b) (int)compareStrings(a, b)
 #define memcmp(a, b, length) (int)compareStart(a, b, length)
 #define memcpy(destination, source, count) ({ copyMemory8((uint8_t*)(source), (uint8_t*)(destination), count); 0; })
+#define memset(destination, value, count) ({ setMemory8((uint8_t*)(destination), value, count); 0; })
+#define strcpy(destination, source) ({ copyString(source, destination); 0; })
